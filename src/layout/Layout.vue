8b2281b47ab90e0c11ea47f70e3e8abd2c225326
Yosiak-alv <Josias_romero60@outlook.com>
2024-05-27 13:06:18 -0600

    Create Layout.vue

---
+++ src/layout/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavBar from '../components/core/nav/NavBar.vue';
+import Footer from '../components/core/Footer.vue';
+</script>
+
+<template>
+    <div class="mx-auto max-w-screen-2xl">
+        <!-- Navigation -->
+        <NavBar />
+        <!-- Page Content -->
+        <main>
+            <slot />
+        </main>
+        <!-- Footer -->
+        <Footer />
+    </div>
+</template>
