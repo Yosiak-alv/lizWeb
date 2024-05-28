971d7aab46d4e8298592949c704e2ccbbdb2704e
Yosiak-alv <Josias_romero60@outlook.com>
2024-05-27 22:05:22 -0600

    Update Projects View

---
+++ src/components/projects/CardProject.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps({
+        project: Object,
+        required: true
+    });
+</script>
+
+<template>
+    <div class="max-w-sm bg-white border shadow">
+        <a href="#">
+            <img :src="props.project.image" />
+        </a>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{project.title}}</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 ">
+                {{project.description}}
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/views/Projects.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { projectsData } from '../utils/data';
+import CardProject from '../components/projects/CardProject.vue';
+
+</script>
+<template>
+    <div class="mt-20 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="max-w-screen-lg text-gray-500 sm:text-lg ">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold  text-gray-900">Mis Proyectos</h2>
+            <p class="mb-4 font-light">
+                Aquí encontrarás algunos de los proyectos en los que he trabajado. 
+                Si deseas saber más sobre alguno de ellos, no dudes en contactarme.
+            </p>
+        </div>
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pt-2">    
+            <CardProject v-for="project in projectsData()" :key="project.id" :project="project" />
+        </div>
+    </div>
+</template>
