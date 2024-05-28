86ccd51ab6a98366a3cc7cd88d119ce5fb2af3a6
Yosiak-alv <Josias_romero60@outlook.com>
2024-05-27 22:04:29 -0600

    Create data.js

---
+++ src/utils/data.js
@@ -0,0 +1,178 @@
+import certificate from '../assets/icons/certificate.svg';
+import insta from '../assets/icons/insta.svg';
+import linkedin from '../assets/icons/linkedin.svg';
+import mail from '../assets/icons/mail.svg';
+import phone from '../assets/icons/phone.svg';
+import map from '../assets/icons/map.svg';
+
+const servicesData = () => {
+    return [
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        },
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        },
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        },
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        },
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        },
+        {
+            title: 'SEO Services',
+            description: 'This is a wider card with supporting text below as a natural content.',
+            icon: certificate
+        }
+    ];
+}
+
+const testimonialData = () => { 
+    return [
+        {
+            id: 1,
+            name: 'Jane Doe',
+            title: 'Product Designer',
+            image: 'https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp',
+            content: 'LLorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus.'
+        },
+        {
+            id: 2,
+            name: 'John Doe',
+            title: 'Product Manager',
+            image: 'https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus.'
+        },
+        {
+            id: 3,
+            name: 'John Doe',
+            title: 'Product Manager',
+            image: 'https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus.'
+        }
+    ];
+}
+
+const contactData = () => {
+    return [
+        {
+            id: 1,
+            title: "Correo", 
+            description: "Puedes Contactarme por", 
+            href: "mailto:liz.alvare@gmail.com", 
+            hrefContent: "liz.alvare@gmail.com",
+            icon: mail
+        },
+        {
+            id: 2,
+            title: "Telefono", 
+            description: "Puedes Contactarme por", 
+            href: "tel:+50377493748",
+            hrefContent: "+503(77493748)",
+            icon: phone
+        },
+        {
+            id: 3,
+            title:"Linkedin",
+            description:"Agregame a tu red", 
+            href:"https://www.linkedin.com/in/elizabeth-alvarenga-/",
+            hrefContent:"Elizabeth Alvarenga",
+            icon: linkedin
+        },
+        {
+            id:4,
+            title:"Instagram", 
+            description:"Búscame como",
+            href:"https://www.instagram.com/liz_alvare?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==", 
+            hrefContent:"@liz_alvare",
+            icon: insta
+        },
+        {
+            id:5,
+            title:"Ubicacion",
+            description:"Puedes encontrarme en",
+            hrefContent:"Antiguo Cuscatlán, La Libertad, El Salvador.",
+            icon: map
+        }
+    ];
+}
+
+const slidesData = () => {
+    return [
+        {
+            id: 1,
+            imgSrc: 'https://i.pinimg.com/originals/3b/57/2d/3b572d0454acd0be60331f060e9c1743.webp',
+            title: 'Elaboración de planos arquitectónicos',
+            description: 'Planos, detalles constructivos, trámites y permisos para cualquier tipo de proyectos.'
+        },
+        {
+            id: 2,
+            imgSrc: 'https://media.admagazine.com/photos/618a709a72049e253173ee7c/master/w_1600%2Cc_limit/56036.jpg',
+            title: 'Desarrollo Arquitectónico',
+            description: 'Desarrollo arquitectónico para ambientes interiores y exteriores con materiales de alta calidad.'
+        },
+        {
+            id: 3,
+            imgSrc: 'https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2023/08/naveindustrial.jpg',
+            title: 'Desarrollo de proyectos industriales',
+            description: 'Naves industriales, plantas, parques industriales y parques de servicios.'
+        }
+    ];
+}
+
+const projectsData = () => {
+    return [
+        {
+            id: 1,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        },
+        {
+            id: 2,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        },
+        {
+            id: 3,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        },
+        {
+            id: 4,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        },
+        {
+            id: 5,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        },
+        {
+            id: 6,
+            title: 'Noteworthy technology acquisitions 2021',
+            description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+            image: 'https://www.talasur.com/img/upload/1_43_exteriores-piscina%20(1).png'
+        }
+    ];
+}
+
+export {servicesData, testimonialData, contactData, slidesData, projectsData}
+
