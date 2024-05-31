04864e5b8e105eb34ebf4f29db3075746b2aa4ac
Yosiak-alv <Josias_romero60@outlook.com>
2024-05-30 21:36:09 -0600

    Adding toast and delete Nprogress and swiper

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
 import router from './router'
+import Toast from "vue-toastification";
+
 import './style.css'
-import '../node_modules/nprogress/nprogress.css' 
+import "vue-toastification/dist/index.css";
 import App from './App.vue'
 
 
@@ -10,4 +12,20 @@ const app = createApp(App)
 
 app.use(MotionPlugin)
 app.use(router)
+
+app.use(Toast ,{
+    position: "top-right",
+    timeout: 4000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+})
+
 app.mount('#app')
