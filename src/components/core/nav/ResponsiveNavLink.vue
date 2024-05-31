5168b7413235c574f9db1653f3fd5f45a3bfff9d
Yosiak-alv <Josias_romero60@outlook.com>
2024-05-30 21:40:22 -0600

    Update ResponsiveNavLink.vue

--- src/components/core/nav/ResponsiveNavLink.vue
+++ src/components/core/nav/ResponsiveNavLink.vue
@@ -19,7 +19,7 @@ const handleClick = () => {
 
 const classes = computed(() =>
     props.active
-        ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-gray-600 text-left text-base font-medium text-gray-200 bg-gray-900/50 focus:outline-none focus:text-gray-200 focus:bg-gray-900 focus:border-gray-300 transition duration-150 ease-in-out'
+        ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-green-600 text-left text-base font-medium text-green-200 bg-green-900/50 focus:outline-none focus:text-green-200 focus:bg-green-900 focus:border-green-300 transition duration-150 ease-in-out'
         : 'block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-700 hover:text-gray-200 hover:bg-gray-400 hover:border-gray-600 focus:outline-none focus:text-gray-200 focus:bg-gray-400 focus:border-gray-600 transition duration-150 ease-in-out'
 );
 </script>
